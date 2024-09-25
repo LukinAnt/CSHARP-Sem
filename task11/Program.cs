09adaec683b41e5904332ccc445b86673b0e30e4
Lukin Lukina <a.lukin@rambler.ru>
2024-09-25 21:27:51 +0300

    добавили 11

---
+++ task11/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int number = new Random().Next(10, 1000);
+System.Console.WriteLine(number);
+
+int first = number / 100;
+int second = number % 10;
+
+//int number3 = first * 10 + second;
+//System.Console.WriteLine(number3);
+
+System.Console.WriteLine(first * 10 + second);
