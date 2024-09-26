981e36f2372c632ce37ca6be920aa89495ce8179
Lukin Lukina <a.lukin@rambler.ru>
2024-09-26 09:03:36 +0300

    task12

--- task00/obj/Debug/net9.0/task00.AssemblyInfo.cs
+++ task00/obj/Debug/net9.0/task00.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task00")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09adaec683b41e5904332ccc445b86673b0e30e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("task00")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task00")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ task12/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+12.Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому.
+Если второе число некратно первому, то программа выводит остаток от деления.
+34, 5 -> некратно, остаток 4
+16, 4 -> кратно
+*/
+
+Console.WriteLine("введите 2 числa: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 % number2 == 0) 
+{ 
+    Console.WriteLine("кратно"); 
+}   
+else 
+{ Console.WriteLine("некратно, остаток " + number1 % number2); }
