4367dfdd0ec05a353f0faa46c91cfe1e1426a2c9
Lukin Lukina <a.lukin@rambler.ru>
2024-09-29 20:29:42 +0300

    добавили 16

---
+++ task16/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+16.Напишите программу, которая принимает на вход два числа и проверяет,
+является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет
+*/
+
+
+Console.WriteLine("введите 2 числa: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+//if (number1 * number1 == number2 || number2 * number2 == number1) 
+if (Math.Pow (number1, 2) == number2 || Math.Pow (number2, 2) == number1) // возведение в степень
+{ 
+    Console.WriteLine("да"); 
+}   
+else 
+{ Console.WriteLine("нет"); }
