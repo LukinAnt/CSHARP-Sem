a1d83e392dd18a128693c4bd0d7217d69f45177d
Lukin Lukina <a.lukin@rambler.ru>
2024-10-02 12:45:56 +0300

    task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -6,5 +6,16 @@
 32679 -> 6
 */
 
-Console.WriteLine("введите трех и более значное число число : ");
+Console.WriteLine("введите трех и более значное число число:");
 int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number.ToString().Length;
+if (number2 >= 3) {
+	while (number > 999)
+	{
+		number = number / 10;
+	}
+	int result = number % 10;
+	Console.WriteLine("третье число " + result);
+} else {
+	Console.WriteLine("третьей цифры нет");
+}
